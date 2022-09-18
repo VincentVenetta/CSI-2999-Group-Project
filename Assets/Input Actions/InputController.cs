12a32ed7de7520f105f2171abc6ecd5b20fd38b8
VincentVenetta <88519440+VincentVenetta@users.noreply.github.com>
2022-09-18 03:19:34 -0400

    Implemented basic player input v0.0.3
    Implemented an easily extensible way to add controls for actions the player will perform.

---
+++ Assets/Input Actions/InputController.cs
@@ -0,0 +1,219 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Input Actions/InputController.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @InputController : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @InputController()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""InputController"",
+    ""maps"": [
+        {
+            ""name"": ""PlayerActions"",
+            ""id"": ""dfd11309-a421-46f1-b9af-ad7a003a347d"",
+            ""actions"": [
+                {
+                    ""name"": ""Up"",
+                    ""type"": ""Button"",
+                    ""id"": ""73628243-7476-420e-9249-d05c5ad1675d"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Down"",
+                    ""type"": ""Button"",
+                    ""id"": ""390ef189-98f2-4dfe-8b9d-fa091cd419ca"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Left"",
+                    ""type"": ""Button"",
+                    ""id"": ""2c3fbd33-3ce2-4784-b54b-0074b3441eb5"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""Right"",
+                    ""type"": ""Button"",
+                    ""id"": ""82dc8426-c9b6-448c-9a7c-e104db5832a2"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""23d34b5e-e2fa-44b7-83c4-9322b25b0df9"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Up"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""78e1de91-32f7-441b-896d-1707051d01a6"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Down"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""31f98d81-bdf8-4b58-b39f-f064bb1308d1"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Left"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""c8644ec1-3bc3-4e23-8c6d-bcf1c2d2a601"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Right"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // PlayerActions
+        m_PlayerActions = asset.FindActionMap("PlayerActions", throwIfNotFound: true);
+        m_PlayerActions_Up = m_PlayerActions.FindAction("Up", throwIfNotFound: true);
+        m_PlayerActions_Down = m_PlayerActions.FindAction("Down", throwIfNotFound: true);
+        m_PlayerActions_Left = m_PlayerActions.FindAction("Left", throwIfNotFound: true);
+        m_PlayerActions_Right = m_PlayerActions.FindAction("Right", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // PlayerActions
+    private readonly InputActionMap m_PlayerActions;
+    private IPlayerActionsActions m_PlayerActionsActionsCallbackInterface;
+    private readonly InputAction m_PlayerActions_Up;
+    private readonly InputAction m_PlayerActions_Down;
+    private readonly InputAction m_PlayerActions_Left;
+    private readonly InputAction m_PlayerActions_Right;
+    public struct PlayerActionsActions
+    {
+        private @InputController m_Wrapper;
+        public PlayerActionsActions(@InputController wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Up => m_Wrapper.m_PlayerActions_Up;
+        public InputAction @Down => m_Wrapper.m_PlayerActions_Down;
+        public InputAction @Left => m_Wrapper.m_PlayerActions_Left;
+        public InputAction @Right => m_Wrapper.m_PlayerActions_Right;
+        public InputActionMap Get() { return m_Wrapper.m_PlayerActions; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActionsActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActionsActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsActionsCallbackInterface != null)
+            {
+                @Up.started -= m_Wrapper.m_PlayerActionsActionsCallbackInterface.OnUp;
+                @Up.performed -= m_Wrapper.m_PlayerActionsActionsCallbackInterface.OnUp;
+                @Up.canceled -= m_Wrapper.m_PlayerActionsActionsCallbackInterface.OnUp;
+                @Down.started -= m_Wrapper.m_PlayerActionsActionsCallbackInterface.OnDown;
+                @Down.performed -= m_Wrapper.m_PlayerActionsActionsCallbackInterface.OnDown;
+                @Down.canceled -= m_Wrapper.m_PlayerActionsActionsCallbackInterface.OnDown;
+                @Left.started -= m_Wrapper.m_PlayerActionsActionsCallbackInterface.OnLeft;
+                @Left.performed -= m_Wrapper.m_PlayerActionsActionsCallbackInterface.OnLeft;
+                @Left.canceled -= m_Wrapper.m_PlayerActionsActionsCallbackInterface.OnLeft;
+                @Right.started -= m_Wrapper.m_PlayerActionsActionsCallbackInterface.OnRight;
+                @Right.performed -= m_Wrapper.m_PlayerActionsActionsCallbackInterface.OnRight;
+                @Right.canceled -= m_Wrapper.m_PlayerActionsActionsCallbackInterface.OnRight;
+            }
+            m_Wrapper.m_PlayerActionsActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Up.started += instance.OnUp;
+                @Up.performed += instance.OnUp;
+                @Up.canceled += instance.OnUp;
+                @Down.started += instance.OnDown;
+                @Down.performed += instance.OnDown;
+                @Down.canceled += instance.OnDown;
+                @Left.started += instance.OnLeft;
+                @Left.performed += instance.OnLeft;
+                @Left.canceled += instance.OnLeft;
+                @Right.started += instance.OnRight;
+                @Right.performed += instance.OnRight;
+                @Right.canceled += instance.OnRight;
+            }
+        }
+    }
+    public PlayerActionsActions @PlayerActions => new PlayerActionsActions(this);
+    public interface IPlayerActionsActions
+    {
+        void OnUp(InputAction.CallbackContext context);
+        void OnDown(InputAction.CallbackContext context);
+        void OnLeft(InputAction.CallbackContext context);
+        void OnRight(InputAction.CallbackContext context);
+    }
+}
